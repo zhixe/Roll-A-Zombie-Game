7280aa7a13897334561fbcd0df798d95aeb2a81c
zhixe <amsyar1989@gmail.com>
2017-06-04 12:38:50 +0800

    fix some bugs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class GameManager : MonoBehaviour {
-
+public class GameManager : MonoBehaviour
+{
+    private int selectedZombiePosition=0;
     public GameObject selectedZombie;
     public List<GameObject> zombies;
     public Vector3 selectedSize;
@@ -16,10 +17,58 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
 
-    void SelectZombie(GameObject newZombie){
+        if (Input.GetKeyDown("left"))
+        {
+            GetZombieLeft();
+        }
+
+        if (Input.GetKeyDown("right"))
+        {
+            GetZombieRight();
+        }
+
+        if (Input.GetKeyDown("up"))
+        {
+
+        }
+    }
+
+    void GetZombieLeft()
+    {
+        if (selectedZombiePosition == 0)
+        {
+            selectedZombiePosition = 3;
+            SelectZombie(zombies[3]);
+        }
+        else
+        {
+            selectedZombiePosition = selectedZombiePosition - 1;
+            GameObject newZombie = zombies[selectedZombiePosition];
+            SelectZombie(newZombie);
+        }
+    }
+
+    void GetZombieRight()
+    {
+        if (selectedZombiePosition == 3)
+        {
+            selectedZombiePosition = 0;
+            SelectZombie(zombies[0]);
+        }
+        else
+        {
+            selectedZombiePosition = selectedZombiePosition + 1;
+            SelectZombie(zombies[selectedZombiePosition]);
+        }
+     }
+
+ 
+
+    void SelectZombie(GameObject newZombie)
+    {
+        selectedZombie.transform.localScale = defaultSize;
+        selectedZombie = newZombie;
         newZombie.transform.localScale = selectedSize;
     }
 }
